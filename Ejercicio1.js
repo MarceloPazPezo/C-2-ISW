381455743e9c40155e05ea4ef931a7e6465ba7e2
MarceloPazPezo <marcelo.paz2101@alumnos.ubiobio.cl>
2024-09-08 23:54:38 -0300

    ADD: Variables y tipos de datos

---
+++ Ejercicio1.js
@@ -0,0 +1,12 @@
+// Variables y Tipos de Datos
+let nombre = 'Pepito'
+let edad = 22
+let puedeConducir = true
+
+console.log('Nombre:', nombre)
+console.log('Edad:', edad)
+console.log('¿Puede conducir?', puedeConducir)
+
+console.log(typeof nombre)
+console.log(typeof edad)
+console.log(typeof puedeConducir)
