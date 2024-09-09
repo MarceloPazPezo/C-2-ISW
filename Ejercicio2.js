99a6726a912732d6acfc1db5260ee19ced6767cd
MarceloPazPezo <marcelo.paz2101@alumnos.ubiobio.cl>
2024-09-08 23:55:16 -0300

    ADD: Operadores aritméticos

---
+++ Ejercicio2.js
@@ -0,0 +1,18 @@
+// Operadores Aritméticos
+const numero1 = 20
+const numero2 = 40
+
+const suma = numero1 + numero2
+console.log(numero1 + ' + ' + numero2 + ' = ' + suma)
+
+const resta = numero1 - numero2
+console.log(numero1 + ' - ' + numero2 + ' = ' + resta)
+
+const multiplicacion = numero1 * numero2
+console.log(numero1 + ' x ' + numero2 + ' = ' + multiplicacion)
+
+const division = numero1 / numero2
+console.log(numero1 + ' / ' + numero2 + ' = ' + division)
+
+const modulo = numero2 % numero1
+console.log(numero2 + ' % ' + numero1 + ' = ' + modulo)
