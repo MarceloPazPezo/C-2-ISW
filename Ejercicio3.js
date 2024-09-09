a1d79262323da835d6ae91efce9659b44b2a6934
MarceloPazPezo <marcelo.paz2101@alumnos.ubiobio.cl>
2024-09-09 00:43:36 -0300

    ADD: Operadores de asignación y comparación

---
+++ Ejercicio3.js
@@ -0,0 +1,11 @@
+// Operadores de asignación y comparación
+const numero1 = 15
+const numero2 = 20
+const numero3 = '25'
+
+console.log('¿El numero1 es mayor o igual a numero2? :', numero1 >= numero2)
+console.log('¿El numero1 es menor o igual a numero2? :', numero1 <= numero2)
+console.log('¿El numero1 es menor que numero3? :', numero1 < numero3)
+console.log('¿El numero3 es menor que numero2? :', numero3 < numero2)
+console.log('¿El numero3 es estrictamente diferente que numero1? :', numero3 !== numero1)
+console.log('¿El numero1 es estrictamente igual que numero2? :', numero1 === numero2)
